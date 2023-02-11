cf1b8ef09e7560d4c5746dfd2c67ae5e2d292822
= <=>
2023-02-11 13:24:17 -0600

    Agregado colores institucionales

--- resources/js/App.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div class="h-screen bg-sky-300 font-bold flex justify-center items-center text-8xl text-sky-900 cursor-text flex-col">
-       <div class="mb-4 text-sky-700">UCA Empleos</div>
-       <div class="text-4xl text-sky-500">Tailwind + Vite</div>
-    </div>
-</template>

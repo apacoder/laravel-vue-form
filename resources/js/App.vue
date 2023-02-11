a369988dc97c2bce549a5fc3acf4756a87a1d032
= <=>
2023-02-11 00:53:12 -0600

    Configurando tailwind y Hola mundo

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,3 +1,12 @@
+<script setup>
+import { ref } from 'vue';
+
+const msg = ref('Tailwind & Vite')
+</script>
+
 <template>
-    Hola mundo from Vue
+    <div class="h-screen bg-sky-300 font-bold flex justify-center items-center text-8xl text-sky-900 cursor-text flex-col">
+        <div class="mb-4 text-sky-700">UCA Empleos</div>
+        <div class="text-4xl text-sky-500">{{ msg }}</div>
+    </div>
 </template>
